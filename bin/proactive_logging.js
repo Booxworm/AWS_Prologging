e27f6ef5dce334c873941669c7c38cf553494773
Wonn Jen <lee.wonnjen@gmail.com>
2020-07-18 10:37:27 +0800

    Changes

--- bin/proactive_logging.js
+++ bin/proactive_logging.js
@@ -6,7 +6,7 @@ const { ProactiveLoggingStack } = require('../lib/proactive_logging-stack');
 const app = new cdk.App();
 new ProactiveLoggingStack(app, 'ProactiveLoggingStack', {
     env : {
-        account : 917066274413,
+        account : '917066274413',
         region : 'ap-southeast-1'
     }
 });
--- lambda/logs.js
+++ lambda/logs.js
@@ -2,44 +2,37 @@ const AWS = require('aws-sdk');
 const zlib = require('zlib');
 const fs = require('fs');
 
-AWS.config.update({region: 'ap-southeast-1'});
+AWS.config.update({ region: 'ap-southeast-1' });
 const s3 = new AWS.S3();
 
-const uploadFile = (fileName) => {
-    // Read content from the file
-    const fileContent = fs.readFileSync(fileName);
-
-    // Setting up S3 upload parameters
-    const params = {
-        Bucket: process.env.BUCKET_NAME,
-        Key: 'cat.jpg', // File name you want to save as in S3
-        Body: fileContent
-    };
-
-    // Uploading files to the bucket
-    s3.upload(params, function (err, data) {
-        if (err) {
-            throw err;
-        }
-        console.log(`File uploaded successfully. ${data.Location}`);
-    });
-};
-
-exports.handler = function (input, context) {
-    s3.listBuckets(function (err, data) {
-        if (err) {
-            console.log("Error", err);
-        } else {
-            console.log("Success", data.Buckets);
-        }
-    });
+exports.handler = (input, context) => {
 
     var payload = Buffer.from(input.awslogs.data, 'base64');
     zlib.gunzip(payload, function (e, result) {
         if (e) {
             context.fail(e);
         } else {
-            // uploadFile('../test.txt');
+            result = JSON.parse(result.toString('ascii'));
+            console.log('Event Data:', JSON.stringify(result, null, 2));
+            console.log(result.logEvents[0].message);
+
+            const buf = Buffer.from(JSON.stringify(result));
+            // Setting up S3 upload parameters
+            const params = {
+                Bucket: process.env.BUCKET_NAME,
+                Key: 'logs_'+ Date.now() +'.json', // File name you want to save as in S3
+                Body: buf
+            };
+
+            // Uploading files to the bucket
+            s3.putObject(params, (err, data) => {
+                if (err)
+                    console.log(err, err.stack);
+                else {
+                    console.log('Upload success');
+                    context.succeed(); 
+                }
+            });
 
             // s3.listObjects({
             //     Bucket: process.env.BUCKET_NAME,
@@ -69,7 +62,6 @@ exports.handler = function (input, context) {
             //         console.log(data);
             // });
 
-            context.succeed();
         }
     });
 };
